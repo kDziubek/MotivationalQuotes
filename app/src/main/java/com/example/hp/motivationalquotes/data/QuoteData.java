755e1d7a200e9216763563e83c47bb7c482886a9
KDziubek <dinozaury1@gmail.com>
2018-11-07 15:25:59 +0100

    controller, data and model packages created. setting Volley-library

---
+++ app/src/main/java/com/example/hp/motivationalquotes/data/QuoteData.java
@@ -0,0 +1,16 @@
+package com.example.hp.motivationalquotes.data;
+
+import com.example.hp.motivationalquotes.model.Quote;
+
+import java.util.ArrayList;
+
+public class QuoteData {
+
+    ArrayList<Quote> quoteArrayList = new ArrayList<Quote>();
+
+    public void getQuotes(){
+
+
+    }
+
+}
