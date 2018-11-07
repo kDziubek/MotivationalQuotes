1c9ff73afa262f1a60294e73dbf5995eccbf6e39
KDziubek <dinozaury1@gmail.com>
2018-11-07 18:23:26 +0100

    App controller added... // pasted

--- app/src/main/java/com/example/hp/motivationalquotes/data/QuoteData.java
+++ app/src/main/java/com/example/hp/motivationalquotes/data/QuoteData.java
@@ -9,6 +9,10 @@ public class QuoteData {
     ArrayList<Quote> quoteArrayList = new ArrayList<Quote>();
 
     public void getQuotes(){
+        String url = "https://gist.github.com/b1nary/ea8fff806095bcedacce";
+
+        // creation of JSOn array request
+
 
 
     }
