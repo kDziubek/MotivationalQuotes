5c516a8ade46a5681a5559631340d5b8917229af
KDziubek <dinozaury1@gmail.com>
2018-11-07 19:39:32 +0100

    adding QuoteFragment and QuotePagerAdapter

---
+++ app/src/main/java/com/example/hp/motivationalquotes/data/QuoteViewPagerAdapter.java
@@ -0,0 +1,27 @@
+package com.example.hp.motivationalquotes.data;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.List;
+
+public class QuoteViewPagerAdapter extends FragmentPagerAdapter {
+
+    private List<Fragment> fragments;
+
+    public QuoteViewPagerAdapter(FragmentManager fm, List<Fragment> fragments) {
+        super(fm);
+        this.fragments = fragments;
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        return this.fragments.get(i);
+    }
+
+    @Override
+    public int getCount() {
+        return this.fragments.size();
+    }
+}
