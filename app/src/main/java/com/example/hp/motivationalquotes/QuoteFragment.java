ea3376e0030926fbb77da279fd858b564a11b5ab
KDziubek <dinozaury1@gmail.com>
2018-11-07 20:00:40 +0100

    QuoteFragment.java .

--- app/src/main/java/com/example/hp/motivationalquotes/QuoteFragment.java
+++ app/src/main/java/com/example/hp/motivationalquotes/QuoteFragment.java
@@ -3,9 +3,13 @@ package com.example.hp.motivationalquotes;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.hp.motivationalquotes.model.Quote;
 
 
 /**
@@ -25,7 +29,27 @@ public class QuoteFragment extends Fragment {
         // Inflate the layout for this fragment
 
         View quoteView = inflater.inflate(R.layout.fragment_quote,container,false);
+
+        TextView quoteText = quoteView.findViewById(R.id.quoteText);
+        TextView byAuthor = quoteView.findViewById(R.id.byAuthor);
+        CardView cardView = quoteView.findViewById(R.id.cardview);
+        String quote = getArguments().getString("quote");
+        String author = getArguments().getString("author");
+
+        quoteText.setText(quote);
+        byAuthor.setText("-" + author);
+
         return quoteView;
     }
 
+    public static final QuoteFragment newInstance(String quote, String author){
+
+        QuoteFragment fragment  = new QuoteFragment();
+        Bundle bundle = new Bundle();
+        bundle.putString("quote",quote);
+        bundle.putString("author", author);
+        fragment.setArguments(bundle);
+
+        return fragment;
+    }
 }
