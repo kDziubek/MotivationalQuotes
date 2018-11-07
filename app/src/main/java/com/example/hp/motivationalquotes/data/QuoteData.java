0b3b3d9402ebab83fef92a40727a0a5e21844e30
KDziubek <dinozaury1@gmail.com>
2018-11-07 18:36:12 +0100

    setting JsonArrayRequest ( Listener onRespond, onErrorRespond)

--- app/src/main/java/com/example/hp/motivationalquotes/data/QuoteData.java
+++ app/src/main/java/com/example/hp/motivationalquotes/data/QuoteData.java
@@ -1,6 +1,13 @@
 package com.example.hp.motivationalquotes.data;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.example.hp.motivationalquotes.controller.AppController;
 import com.example.hp.motivationalquotes.model.Quote;
+
+import org.json.JSONArray;
 
 import java.util.ArrayList;
 
@@ -13,7 +20,20 @@ public class QuoteData {
 
         // creation of JSOn array request
 
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
 
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+
+        AppController.getInstance().addToRequestQueue(jsonArrayRequest);
 
     }
 
