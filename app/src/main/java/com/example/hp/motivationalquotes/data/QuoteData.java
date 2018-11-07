3ff20d2c2ba87d907f73a9392be0ae5de9918842
KDziubek <dinozaury1@gmail.com>
2018-11-07 18:43:32 +0100

    adding JSONobject to onRespond method

--- app/src/main/java/com/example/hp/motivationalquotes/data/QuoteData.java
+++ app/src/main/java/com/example/hp/motivationalquotes/data/QuoteData.java
@@ -8,6 +8,8 @@ import com.example.hp.motivationalquotes.controller.AppController;
 import com.example.hp.motivationalquotes.model.Quote;
 
 import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -23,6 +25,19 @@ public class QuoteData {
         JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, new Response.Listener<JSONArray>() {
             @Override
             public void onResponse(JSONArray response) {
+
+                for(int i = 0; i < response.length(); i++){
+                    try {
+                        JSONObject quoteObject = response.getJSONObject(i);
+                        Quote qoute = new Quote();
+                        qoute.setText(quoteObject.getString("text"));
+                        qoute.setFrom(quoteObject.getString("from"));
+
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
 
             }
         }, new Response.ErrorListener() {
